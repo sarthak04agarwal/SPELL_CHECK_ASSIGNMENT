814d722be394d4b194abba23da813ec54793cffa
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-11-17 13:25:52 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+menu = "Main Menu\n" + "1: Spell Check a Word (Linear Search)\n" + "2: Spell Check a Word (Binary Search)\n" + "3: Spell Check Alice In Wonderland (Linear Search)\n" + "4: Spell Check Alice In Wonderland (Binary Search)\n" + "5: Exit\n"
+
+running = True
+while running:
+    number = ("Please enter a number from")
