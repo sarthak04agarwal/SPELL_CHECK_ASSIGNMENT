b0dae69f75e68e2b71e5ed4d046cf564ddc8535b
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-11-17 21:10:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-menu = "Main Menu\n" + "1: Spell Check a Word (Linear Search)\n" + "2: Spell Check a Word (Binary Search)\n" + "3: Spell Check Alice In Wonderland (Linear Search)\n" + "4: Spell Check Alice In Wonderland (Binary Search)\n" + "5: Exit\n"
+
+
+menu = "Main Menu\n" + "1: Spell Check a Word (Linear Search)\n" + "2: Spell Check a Word (Binary Search)\n" + "3: Spell Check Alice In Wonderland (Linear Search)\n" + "4: Spell Check Alice In Wonderland (Binary Search)\n" + "5: Exit\n Please type your number: ""
 
 running = True
 while running:
-    number = ("Please enter a number from")
+    selection = input(menu)
+    if selection == "1":
+        #display_grades()
+    elif selection == "2":
+        #display_honours()
+    elif selection == "3":
+        #stats()
+    elif selection == "4":
+        #randomize_grades()
+    else:
+        loop = False
+
+
+
+
